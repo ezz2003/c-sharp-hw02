a25488c695ebfb4e96ee01b79f198fabaff81f0b
ezz2003 <ezz2003@yandex.ru>
2023-02-17 07:19:24 +1000

    solved the problem11

---
+++ problem11/Program.cs
@@ -0,0 +1,14 @@
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное число
+    // и удаляет вторую цифру этого числа.
+
+
+int DelSecondDigit(int num)
+{
+  // int secondDigit = (num / 100) * 10 + num % 10;
+  return (num / 100) * 10 + num % 10;
+}
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine();
+Console.WriteLine($"было: {number}, без второй цифры получилось: {DelSecondDigit(number)}");
+Console.WriteLine();
