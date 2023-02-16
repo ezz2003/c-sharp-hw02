219348be02a276ef3ff9771b6b210ebfb3ca801a
ezz2003 <ezz2003@yandex.ru>
2023-02-17 09:50:12 +1000

    solving the problem12

---
+++ problem12/Program.cs
@@ -0,0 +1,14 @@
+﻿// 12. Напишите программу, которая будет принимать на вход два числа и выводить, является ли первое число кратным второму, и если нет, то выводить остаток от деления.
+// 
+
+string AnswerRemainder(int num1, int num2)
+{
+  int rem = num1 % num2;
+  return rem != 0 ? $"Число {num1} не кратно числу {num2}, остаток {rem}": "";
+}
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(AnswerRemainder(number1, number2));
