2817108343b9b95fc637312874686f7afeca9417
ezz2003 <ezz2003@yandex.ru>
2023-02-17 12:29:19 +1000

    fixed a bug in problem16

--- problem16/Program.cs
+++ problem16/Program.cs
@@ -3,16 +3,16 @@
 
 
 
-bool Answer(int num1, int num2) => num1 * num1 == num2;
+bool Answer(int num1, int num2) => num1 * num1 == num2 | num2 * num2 == num1;
 
 Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (Answer(number1, number2)){
-  Console.WriteLine($"Число {number2} является квадратом числа {number1}");
+  Console.WriteLine("Одно число является квадратом другого числа");
 }
 else
 {
-  Console.WriteLine($"Число {number2} НЕ является квадратом числа {number1}");
+  Console.WriteLine("Одно число Не является квадратом другого числа");
 }
